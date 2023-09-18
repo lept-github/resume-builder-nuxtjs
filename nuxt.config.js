2c09d61d69e1fa7c2974916727060b33f518ac2d
Luis Pacheco <lept.fl.us@gmail.com>
2023-09-18 19:22:17 -0400

    adding google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,23 @@ export default {
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    script: [
+      {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-50535DDLXT',
+      },
+      {
+        innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          window.dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-50535DDLXT');
+      `,
+        type: 'text/javascript',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
